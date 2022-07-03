86b9bd49d0aa8789f6f105e8040fc245132c6b40
AmirovAldiar <amirovaldiar@gmail.com>
2022-07-03 23:34:31 +0300

    ver 0.1

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,112 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div class="content">
+		<div class="left"/>																
+		<div class="center">
+			<img class="image" src="./assets/kek.jpg">
+			<div class="image-shadow"/>
+			<div class="info">
+				<div class="name">
+					[ALDIYAR AMIROV]
+				</div>
+				<div class="position">
+					[golang dev.]
+				</div>
+			</div>
+			<div class="links">
+				<a class="link" href="https://github.com/AmirovAldiyar">
+					{github}
+				</a>
+				<a class="link" href="https://t.me/AmirovAldiyar">
+					{telegram}
+				</a>
+				<a class="link" href="https://www.linkedin.com/in/aldiyar-amirov-b04037197/">
+					{linked.in}
+				</a>
+				<a class="link" href="https://www.instagram.com/aldeeyar/">
+					{instagram}
+				</a>
+				<a class="link" href="mailto:amirovaldiar@gmail.com">
+					{e-mail}
+				</a>
+			</div>
+		</div>
+		<div class="right"/>																
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Courier+Prime&display=swap');
+body {
+	font-family: 'Courier Prime', monospace;
+}
+.content {
+	font-size: 24px;
+	width: 360px;
+	height: 450px;
+	display: flex;
+	flex-direction: row;
+	position: absolute;
+	left: 50%;
+	top: 50%;
+	transform: translate(-50%, -50%);
+}
+.left {
+	width: 30px;
+	height: 450px;
+	border-bottom: 4px solid black;
+	border-left: 4px solid black;
+	border-top: 4px solid black;
+}
+.right {
+	width: 30px;
+	height: 450px;
+	border-bottom: 4px solid black;
+	border-right: 4px solid black;
+	border-top: 4px solid black;
+}
+.center {
+	width: 390px;
+	height: 700px;
+	display: flex;
+	flex-direction: column;
+}
+.info {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	gap: 8px;
+}
+.links {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	gap: 12px;
+	margin-top: 28px;
+}
+.link {
+	text-decoration: none;
+	color: black;
+}
+.image {
+	width: 113px;
+	height: 113px;
+	border-radius: 50%;
+	margin: 18px auto;
+}
+.image-shadow {
+	position: absolute;
+	top: 18px;
+	left: 50%;
+	transform: translate(-50%, 0);
+	border-radius: 50%;
+	width: 113px;
+	height: 113px;
+	box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.5) inset;
 }
 </style>
